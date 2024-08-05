2c6cbe2568140d4f731c570a5382602996382392
andzow <andreywsaers@gmail.com>
2024-08-05 10:40:38 +0300

    wdsadsd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,18 @@ export default defineNuxtConfig({
     "/admin": { ssr: false },
     "/login": { ssr: false },
   },
-
   app: {
     head: {
+      meta: [
+        {
+          name: "yandex-verification",
+          content: "2c385f0c2c03ec3b"
+        },
+        {
+          name: "google-site-verification",
+          content: "qFXRR5p2K_9bdIOnZEuxOsKpkTGLsTqR8aCeXqJRm8I"
+        }
+      ],
       title: "Разработка сайтов и web приложений | waveStudio",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
